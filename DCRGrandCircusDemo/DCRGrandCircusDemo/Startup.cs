53369b302432d383234230afc5a9f13796faeb6a
a2davidr <roedavidc@yahoo.com>
2014-06-25 14:58:14 -0400

    New Demo
    This is the first demo

---
+++ DCRGrandCircusDemo/DCRGrandCircusDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DCRGrandCircusDemo.Startup))]
+namespace DCRGrandCircusDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
